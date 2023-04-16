52484918ea42cab3865627caac8f86d794080cba
Mizuki-Takewaka <mizuki1245.m@gmail.com>
2023-04-17 01:30:52 +0900

    タスクの追加と一覧表示

--- src/resources/js/pages/Example.jsx
+++ src/resources/js/pages/Example.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 function Example() {
     const now = new Date();
@@ -8,6 +9,22 @@ function Example() {
     const hour = now.getHours();
     const minute = now.getMinutes();
     const dateAndTime = `${year}/${month}/${date} ${hour}:${minute}`;
+
+    //todoリストをuseStateに格納
+    const [todos, setTodos] = useState([]);
+    //入力値をuseStateに格納
+    const [task, setTask] = useState('');
+
+    const handleOnChange = (event) => {
+        setTask( event.target.value )
+    }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if(task === '') return
+        setTodos(todos => [...todos, task])
+        setTask('')
+    }
 
     return (
         <div className="container">
@@ -21,25 +38,27 @@ function Example() {
                         </div>
 
                         <div className="card-body">
-
-                            <form action="">
+                            <form action="" onSubmit={handleSubmit}>
                                 <div class="d-flex bd-highlight">
                                     <div class="p-2 flex-grow-1 bd-highlight">
-                                        <input type="text" class="form-control"/>
+                                        <input type="text" class="form-control" value={ task } onChange={handleOnChange}/>
                                     </div>
-                                    <div class="p-2  bd-highlight">
+                                    <div class="p-2 bd-highlight">
                                         <button type="button submit" className="btn btn-outline-primary">追加</button>
                                     </div>
                                 </div>
                             </form>
 
                             <ul>
-                                {/* { todos.map((todo, index) => (
-                                    <li key={ index }>{ todo.task }</li>
-                                ))} */}
+                                { todos.map((todo, index) => (
+                                    <li key={ index }>
+                                        {
+                                        todo
+                                        }
+                                    </li>
+                                ))}
                             </ul>
                             <button type="button" className="btn btn-outline-danger">削除</button>
-                            
                         </div>
 
                     </div>
