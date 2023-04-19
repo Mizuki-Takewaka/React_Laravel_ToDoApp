df0a0feb31c76d37491d773afd9d99d9c3ed5def
Mizuki-Takewaka <mizuki1245.m@gmail.com>
2023-04-19 19:00:19 +0900

    Taskテーブル用Model,Controller,APIルート作成

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(TaskController::class)->group(function () {
+    Route::get('/tasks', 'index');
+    Route::post('/tasks', 'store');
 });
